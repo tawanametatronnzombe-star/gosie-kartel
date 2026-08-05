3bcf38a7bbf52442addce45b8ff0fdd5f797b038
tawanametatronnzombe-star <tawanametatronnzombe@gmail.com>
2026-08-05 14:36:32 +0200

    Fix string formatting for Supabase URL and key

--- supabase.js
+++ supabase.js
@@ -1,9 +1,10 @@
-const SUPABASE_URL = https://tnlktzagziuwjjzgrrna.supabase.co;
+const SUPABASE_URL = "https://tnlktzagziuwjjzgrrna.supabase.co";
 
-const SUPABASE_ANON_KEY =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRubGt0emFneml1d2pqemdycm5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5MjA5MzIsImV4cCI6MjEwMTQ5NjkzMn0.uuj1wWwG8DfhCK8bqvzoGIaxuhDwlrNIxXwAL9jkd2c;
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRubGt0emFneml1d2pqemdycm5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5MjA5MzIsImV4cCI6MjEwMTQ";
 
 const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 );
+
 window.supabaseClient = supabaseClient;
